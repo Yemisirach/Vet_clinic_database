5c61587ae6cab950f8dc79a622e1923048a2790e
Yemisirach <yemsrach3723@gmail.com>
2023-04-11 12:32:53 +0300

    Making the id column not null and autoincremented

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id              INT,
+    id int NOT NULL IDENTITY(1,1),
     name            CHAR(20),
     date_of_birth   DATE,
     escape_attempts INT, 
