279e27c7a6bf521bd264cf593dd60121e93d5cac
Yemisirach <yemsrach3723@gmail.com>
2023-04-12 07:50:16 +0300

    Update animal table

--- queries.sql
+++ queries.sql
@@ -8,3 +8,32 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.3 AND 17.4;
+
+
+/* Day 2 */
+
+/* Transaction 1 */
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+UPDATE animals SET species = 'digimon' WHERE name ilike '%mon%';
+UPDATE animals SET species = 'pokemon' WHERE species is null;
+COMMIT;
+
+/* Transaction 2 */
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = (weight_kg * (-1));
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg = (weight_kg * (-1)) WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT (*) FROM animals;
+SELECT COUNT (*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered,SUM(escape_attempts) FROM animals GROUP BY neutered;
+SELECT neutered,MIN(weight_kg),MAX(weight_kg) FROM animals GROUP BY neutered;
+SELECT neutered,AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY neutered;
