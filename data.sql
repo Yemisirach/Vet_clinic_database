93af94ee2ba50364ee70da7d71b38d43e79277fd
Yemisirach <yemsrach3723@gmail.com>
2023-04-14 07:44:28 +0300

    query multiple tables

--- data.sql
+++ data.sql
@@ -12,3 +12,36 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (9, 'Boarmon', '2005-06-07', 7, true, 20.4);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (10, 'Blossom', '1998-10-13', 3, true, 17);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (11, 'Ditto', '2022-05-14', 4, true, 22);
+
+INSERT INTO animals VALUES 
+  (5, 'Charmander', '02-08-2020', 0, 'False', -11),
+  (6, 'Plantmon', '11-15-2021', 2, 'True', -5.7),
+  (7, 'Squirtle', '04-02-1993', 3, 'False', -12.13),
+  (8, 'Angemon', '06-12-2005', 1, 'True', -45),
+  (9, 'Boarmon', '06-07-2005', 7, 'True', 20.4),
+  (10, 'Blossom', '10-13-1998', 3, 'True', 17),
+  (11, 'Ditto', '05-14-2022', 4, 'True', 22);
+
+INSERT INTO owners (full_name, age) VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES
+  ('Pokemon'),
+  ('Digimon');
+
+INSERT INTO animals (name, species_id, owner_id) VALUES
+  ('Agumon', 2, 1),
+  ('Gabumon', 2, 2),
+  ('Pikachu', 1, 2),
+  ('Devimon', 2, 3),
+  ('Plantmon', 1, 3),
+  ('Charmander', 1, 4),
+  ('Squirtle', 1, 4),
+  ('Blossom', 1, 4),
+  ('Angemon', 2, 5),
+  ('Boarmon', 1, 5);
--- schema.sql
+++ schema.sql
@@ -10,3 +10,21 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species CHAR(20);
+CREATE TABLE owners (
+   id SERIAL PRIMARY KEY,
+    full_name TEXT,
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+ALTER TABLE animals
+    ADD COLUMN species_id INTEGER,
+    ADD COLUMN owner_id INTEGER,
+    DROP COLUMN species,
+    ADD FOREIGN KEY (species_id) REFERENCES species (id),
+    ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
+  
